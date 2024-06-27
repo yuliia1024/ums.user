61523e030e2d5937edc630eb73a9d8a2c8129a08
yuliiakovalenko <jk10101999@gmail.com>
2024-06-27 22:42:27 +0300

    added trx

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,7 @@
 import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, DataSource } from 'typeorm';
 import { User } from './user.entity';
-import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { CreateUserDto } from './dto/create-user.dto';
 import { omit } from 'lodash';
 import { CreateUserResponseDto } from './dto/create-user.response.dto';
@@ -11,25 +10,37 @@ import { NotificationService } from '../notification/notification.service';
 @Injectable()
 export class UserService {
   private readonly logger = new Logger(UserService.name);
+
   constructor(
     @InjectRepository(User)
     private usersRepository: Repository<User>,
-    private readonly notificationService: NotificationService
+    private readonly notificationService: NotificationService,
+    private readonly dataSource: DataSource,
   ) {}
 
   async create(dto: CreateUserDto): Promise<CreateUserResponseDto> {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
     let user: User;
 
     try {
       user = this.usersRepository.create(dto);
 
-      await this.usersRepository.save(user);
+      await queryRunner.manager.save(user);
 
       await this.notificationService.send('user.created', omit(user, 'password'));
+
+      await queryRunner.commitTransaction();
     } catch (err) {
-      throw new HttpException(err, HttpStatus.INTERNAL_SERVER_ERROR);
+      await queryRunner.rollbackTransaction();
+      this.logger.error('Error creating user:', err);
+      throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
+    } finally {
+      await queryRunner.release();
     }
 
-    return user
+    return user;
   }
 }
